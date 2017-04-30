7a277607956ff079b1285ff5c135ef0647d84875
F6Systems.com <f6systems@si.ipcloud.net>
2017-04-30 10:16:34 -0600

    a little clean up.

--- handlers_test.go
+++ handlers_test.go
@@ -9,47 +9,38 @@ import (
 )
 
 //TestDude
-func TestDude (t *testing.T) {
+func TestDude(t *testing.T) {
 	log.Printf("[TestDude] - Starting test.\n")
-	        req := httptest.NewRequest("GET","http://sc.ipcloud.com",nil)
+	req := httptest.NewRequest("GET", "http://sc.ipcloud.com", nil)
 
-       //create a ResponseRecorder to record the response
-        rr := httptest.NewRecorder()
-        handler := http.HandlerFunc(Dude)
-        //handler := rootH()
-        //
-    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method 
-    // directly and pass in our Request and ResponseRecorder.
-    handler.ServeHTTP(rr, req)
+	//create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Dude)
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
 
-    if status := rr.Code; status != http.StatusOK {
-    // Check the status code is what we expect. -> StatusTemporaryRedirect (307)
-    //if status := rr.Code; status != http.StatusTemporaryRedirect {
-        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusTemporaryRedirect)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusTemporaryRedirect)
 	}
-        t.Logf("\t[%v] - Dude() tested. Got expected %d = %d.\n","checkMark",http.StatusTemporaryRedirect,rr.Code)
+	t.Logf("\t[%v] - Dude() tested. Got expected %d = %d.\n", "checkMark", http.StatusTemporaryRedirect, rr.Code)
 
 }
 
-func TestIP (t *testing.T) {
-        log.Printf("[TestDude] - Starting test.\n")
-                req := httptest.NewRequest("GET","http://sc.ipcloud.com",nil)
+func TestIP(t *testing.T) {
+	log.Printf("[TestDude] - Starting test.\n")
+	req := httptest.NewRequest("GET", "http://sc.ipcloud.com", nil)
 
-       //create a ResponseRecorder to record the response
-        rr := httptest.NewRecorder()
-        handler := http.HandlerFunc(IP)
-        //handler := rootH()
-        //
-    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-    // directly and pass in our Request and ResponseRecorder.
-    handler.ServeHTTP(rr, req)
+	//create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(IP)
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
 
-    if status := rr.Code; status != http.StatusOK {
-    // Check the status code is what we expect. -> StatusTemporaryRedirect (307)
-    //if status := rr.Code; status != http.StatusTemporaryRedirect {
-        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusTemporaryRedirect)
-        }
-        t.Logf("\t[%v] - IP() tested. Got expected %d = %d.\n","checkMark",http.StatusTemporaryRedirect,rr.Code)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusTemporaryRedirect)
+	}
+	t.Logf("\t[%v] - IP() tested. Got expected %d = %d.\n", "checkMark", http.StatusTemporaryRedirect, rr.Code)
 
 }
-
