46128c12ce48ceea3e56b6f5f5f6353e2e058000
F6Systems.com <f6systems@si.ipcloud.net>
2017-04-30 14:31:52 -0600

    initConfig start

---
+++ initConfig.go
@@ -0,0 +1,11 @@
+//filename=initConfig.go
+//goal=update code base to have a configuration process to run when initialized (starting).
+package main
+
+import (
+	"log"
+)
+
+func init() {
+	log.Printf("[initConfig:init()] - init()\n")
+}
